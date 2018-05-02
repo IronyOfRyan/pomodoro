c986cd952b1ed80195d18e262e2eefbe325079b2
Ryan Paige <oshmagoshzz@gmail.com>
2018-05-02 10:37:05 -0400

    add mode selector css

--- pomo.js
+++ pomo.js
@@ -1,26 +1,26 @@
 const workButton = document.querySelector("#workButton"),
-  breakButton = document.querySelector("#breakButton"),
-  notification = document.querySelector("#notification"),
-  playButton = document.querySelector("#playButton"),
-  pauseButton = document.querySelector("#pauseButton"),
-  resetButton = document.querySelector("#resetButton"),
-  minuteSpan = document.querySelector("#minuteSpan"),
-  secondSpan = document.querySelector("#secondSpan"),
-  workMin = document.querySelector("#workMin"),
-  breakMin = document.querySelector("#breakMin"),
-  incDec = document.querySelectorAll(".incDecButtons"),
-  audio = new Audio("alarm-buzzer.mp3");
+      breakButton = document.querySelector("#breakButton"),
+      notification = document.querySelector("#notification"),
+      playButton = document.querySelector("#playButton"),
+      pauseButton = document.querySelector("#pauseButton"),
+      resetButton = document.querySelector("#resetButton"),
+      minuteSpan = document.querySelector("#minuteSpan"),
+      secondSpan = document.querySelector("#secondSpan"),
+      workMin = document.querySelector("#workMin"),
+      breakMin = document.querySelector("#breakMin"),
+      incDec = document.querySelectorAll(".incDecButtons"),
+      audio = new Audio("alarm-buzzer.mp3");
 
 
 let app = {
 
-  progress: null,
   audio: null,
   minutes: 0,
   seconds: 0,
   interval: null,
   started: false,
   areYouWorking: null,
+  // init that runs deafult settings whenever the page is reloaded
   init: (activity) => {
     if (activity == 'work') {
       notification.innerHTML = 'Get to Work!';
@@ -31,8 +31,9 @@ let app = {
       this.minutes = +breakMin.innerHTML;
     }
     this.seconds = 00;
-    app.renderDom();
+    app.renderTime();
   },
+  // Padding that provides the extra 0 when the number is single digit
   zeroPad: (num) => {
     return (num < 10) ? "0" + num : num;
   },
@@ -44,10 +45,16 @@ let app = {
     notification.classList.remove('hide');
     notification.classList.add('show');
   },
-  renderDom: () => {
+  toggleSelected: (work, rest) => {
+    work.classList.add('selected');
+    rest.classList.remove('selected');
+  },
+  // Renders the timer numbers on callback
+  renderTime: () => {
     minuteSpan.innerHTML = app.zeroPad(this.minutes);
     secondSpan.innerHTML = app.zeroPad(this.seconds);
   },
+  // Reset the value to whatver the work/break buttons value is
   resetVal: () => {
     this.started = false;
     if (this.areYouWorking) {
@@ -56,8 +63,9 @@ let app = {
       this.minutes = parseInt(breakMin.innerHTML);
     }
     this.seconds = 00;
-    app.renderDom();
+    app.renderTime();
   },
+  // Logic that count the timer down from 59 seconds subtracting a minute everytime it reaches 0 until secs/min are both 0
   countDown: () => {
     if (!this.started) {
       return false
@@ -66,7 +74,7 @@ let app = {
     if (this.seconds == 0) {
       if (this.minutes == 0) {
         app.timerDone();
-        audio.play()
+        audio.play();
         return;
       }
       this.seconds = 59;
@@ -74,21 +82,24 @@ let app = {
     } else {
       this.seconds--;
     }
-    app.renderDom();
+    app.renderTime();
   },
   audioStop: () => {
     audio.pause();
     audio.currentTime = 0;
   },
+  // Clears the interval to stop the function from changing the numbers
   timerStop: () => {
     clearInterval(this.interval);
   },
+  // When the timer gets to all 0's switch it from the work timer to the break and vice versa
   timerDone: () => {
     this.minutes = 0;
     this.seconds = 00;
     this.started = false;
     app.init() ? areYouWorking : app.init('work');
   },
+  // Loops through the inc/dec buttons and picks which one is clicked
   inc: incDec.forEach(elem => {
     elem.addEventListener('click', event => {
       // Makes sure the value is always divisible by 5. No accidental countdown starts at 4's, 9's, etc.
@@ -105,7 +116,7 @@ let app = {
         }
 
         workMin.innerHTML = this.minutes -= 5;
-        app.renderDom();
+        app.renderTime();
       } else if (event.target.id == 'workPlus') {
         if (!this.areYouWorking) {
           return
@@ -115,7 +126,7 @@ let app = {
         }
 
         workMin.innerHTML = this.minutes += 5;
-        app.renderDom();
+        app.renderTime();
       }
 
       if (event.target.id == 'breakMinus') {
@@ -142,7 +153,7 @@ let app = {
           event.target.nextElementSibling.innerHTML = '+5';
           breakMin.innerHTML = this.minutes -= 5;
         }
-        app.renderDom();
+        app.renderTime();
       } else if (event.target.id == 'breakPlus') {
         if (this.areYouWorking) {
           return
@@ -161,7 +172,7 @@ let app = {
           event.target.previousElementSibling.innerHTML = '-5';
           event.target.innerHTML = '+5';
         }
-        app.renderDom()
+        app.renderTime()
       }
       app.timerStop();
     });
@@ -172,20 +183,23 @@ let app = {
     notification.innerHTML = 'Get to Work!';
     this.minutes = parseInt(workMin.innerHTML);
     this.areYouWorking = true;
-    app.renderDom();
+    app.toggleSelected(workButton, breakButton);
+    app.renderTime();
     app.audioStop();
+
   }),
 
   breakButton: breakButton.addEventListener('click', () => {
     app.timerStop();
     notification.innerHTML = 'Take a break!';
+    app.toggleSelected(breakButton, workButton);
     app.init();
     app.audioStop();
   }),
 
   playButton: playButton.addEventListener('click', () => {
     if (this.minutes == 0) {
-      return this.seconds = 0
+      return this.seconds = 0;
     }
     if (this.interval) {
       app.timerStop();
